e6edee17b1fad72ce28c5b1c4c0e9e0070c67c63
Adithya Nair <adtya@adtya.xyz>
2023-02-26 12:25:03 +0530

    make users immutable

--- users/default.nix
+++ users/default.nix
@@ -1,8 +1,10 @@
 { pkgs, ... }:
 
 {
+  users.mutableUsers = false;
   users.users = {
     adtya = {
+      hashedPassword = "$y$j9T$WOLGsPx.0lb9nSD4Jf6mz1$SokbnAJ8RV/CmnKe5/8rBt66dRHcW.tFabV1q9ZWlPB";
       description = "Adithya Nair";
       isNormalUser = true;
       extraGroups = [ "wheel" "networkmanager" "docker" "libvirtd" ];
