a6fa52f49420d834decaba092360f73f54823b20
Adithya Nair <adtya@adtya.xyz>
2023-02-28 20:27:58 +0530

    enable uefi

--- system/virtualisation.nix
+++ system/virtualisation.nix
@@ -1,6 +1,8 @@
 { pkgs, ... }: {
   virtualisation.docker.enable = true;
   virtualisation.libvirtd.enable = true;
+  virtualisation.libvirtd.qemu.ovmf.enable = true;
+  virtualisation.libvirtd.qemu.ovmf.packages = [ pkgs.OVMFFull.fd ];
   environment.systemPackages = with pkgs; [
     virt-manager
   ];
