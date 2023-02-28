e69cb989f5d665a01acabc99df1e54c541b7e153
Adithya Nair <adtya@adtya.xyz>
2023-02-28 20:26:49 +0530

    user mount points

--- users/default.nix
+++ users/default.nix
@@ -11,4 +11,41 @@
       shell = pkgs.zsh;
     };
   };
+  fileSystems = {
+    "/home/adtya/Documents" = {
+      device = "/dev/disk/by-label/NIXSYSTEM";
+      fsType = "btrfs";
+      options = [ "subvol=/@data/documents" "compress-force=zstd" ];
+    };
+    "/home/adtya/Downloads" = {
+      device = "/dev/disk/by-label/NIXSYSTEM";
+      fsType = "btrfs";
+      options = [ "subvol=/@data/downloads" "compress-force=zstd" ];
+    };
+    "/home/adtya/Music" = {
+      device = "/dev/disk/by-label/NIXSYSTEM";
+      fsType = "btrfs";
+      options = [ "subvol=/@data/music" "compress-force=zstd" ];
+    };
+    "/home/adtya/Others" = {
+      device = "/dev/disk/by-label/NIXSYSTEM";
+      fsType = "btrfs";
+      options = [ "subvol=/@data/others" "compress-force=zstd" ];
+    };
+    "/home/adtya/Pictures" = {
+      device = "/dev/disk/by-label/NIXSYSTEM";
+      fsType = "btrfs";
+      options = [ "subvol=/@data/pictures" "compress-force=zstd" ];
+    };
+    "/home/adtya/Projects" = {
+      device = "/dev/disk/by-label/NIXSYSTEM";
+      fsType = "btrfs";
+      options = [ "subvol=/@data/projects" "compress-force=zstd" ];
+    };
+    "/home/adtya/Videos" = {
+      device = "/dev/disk/by-label/NIXSYSTEM";
+      fsType = "btrfs";
+      options = [ "subvol=/@data/videos" "compress-force=zstd" ];
+    };
+  };
 }
