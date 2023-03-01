3f7e1a9364a4d2e516ed1c953147ee1485ef8944
Adithya Nair <adtya@adtya.xyz>
2023-03-01 22:52:35 +0530

    hashed password for root

--- users/default.nix
+++ users/default.nix
@@ -3,6 +3,7 @@
 {
   users.mutableUsers = false;
   users.users = {
+    root.hashedPassword = "$y$j9T$xLZV3Xd.xBb3L2iXy1MPD/$eyvDDYu5lMhqXERgkpp0oBItiszp0rmh1siXAMpTJcB";
     adtya = {
       hashedPassword = "$y$j9T$WOLGsPx.0lb9nSD4Jf6mz1$SokbnAJ8RV/CmnKe5/8rBt66dRHcW.tFabV1q9ZWlPB";
       description = "Adithya Nair";
