5b3bd36d8fcea2e6be715ed56042fcd2b2f6785a
Adithya Nair <adtya@adtya.xyz>
2023-03-01 22:32:40 +0530

    open new window in ~

--- home/tmux.nix
+++ home/tmux.nix
@@ -32,7 +32,6 @@
 
       bind - split-window -v -c "#{pane_current_path}"
       bind \\ split-window -h -c "#{pane_current_path}"
-      bind c new-window -c "#{pane_current_path}"
     '';
   };
 }
