22c878b796bd4ad0d8217d7779cb5c4ffae2b470
Adithya Nair <adtya@adtya.xyz>
2023-03-02 15:52:44 +0530

    nix: cleanup substituters

--- system/default.nix
+++ system/default.nix
@@ -13,8 +13,7 @@
   ];
 
   nix.settings.substituters = [
-    "https://nix-community.cachix.org"
-    "https://cache.nixos.org"
+    "https://nix-community.cachix.org/"
   ];
   nix.settings.trusted-public-keys = [
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
