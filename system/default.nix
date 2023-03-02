03c6eb207f1aba5cb2761f4a4ad0f385812df37e
Adithya Nair <adtya@adtya.xyz>
2023-03-02 15:50:09 +0530

    nix: enable community cachix server

--- system/default.nix
+++ system/default.nix
@@ -12,6 +12,13 @@
     ./swaylock.nix
   ];
 
+  nix.settings.substituters = [
+    "https://nix-community.cachix.org"
+    "https://cache.nixos.org"
+  ];
+  nix.settings.trusted-public-keys = [
+    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+  ];
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   nix.settings.auto-optimise-store = true;
   nixpkgs.config.allowUnfree = true;
