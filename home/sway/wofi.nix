0f80f728a12552c6f0e6e8a92e2b37f3aa749a78
Adithya Nair <adtya@adtya.xyz>
2023-03-03 00:13:31 +0530

    programs: rofi

--- home/sway/wofi.nix
+++ home/sway/wofi.nix
@@ -1,11 +1,19 @@
 { pkgs, ... }:
 
 {
-	programs.rofi = {
-		enable = true;
-		package = pkgs.rofi-wayland.override { symlink-dmenu = true; };
-		theme = ./rofi/dracula.rasi;
-	};
+  programs.rofi = {
+    enable = true;
+    package = pkgs.rofi-wayland.override { symlink-dmenu = true; };
+    theme = ./rofi/dracula.rasi;
+    extraConfig = {
+      modi = "drun,run";
+      font = "FiraCode Nerd Font 16";
+      show-icons = true;
+      terminal = "kitty";
+      icon-theme = "Newaita-dark";
+      sort = false;
+    };
+  };
   home.packages = with pkgs; [
     wofi
   ];
