383f4035b1e0f6bc9de78c86e4c491eb14e81e51
Adithya Nair <adtya@adtya.xyz>
2023-03-03 12:31:02 +0530

    sway: use same sway everywhere

--- home/terminal.nix
+++ home/terminal.nix
@@ -32,11 +32,11 @@
       enableCompletion = true;
       enableSyntaxHighlighting = true;
       initExtraBeforeCompInit = ''bindkey -v '^?' backward-delete-char'';
-      #profileExtra = ''
-      #  if [ -z $DISPLAY ] && [ "$(tty)" = "/dev/tty1" ] ; then
-      #    exec ${pkgs.sway}/bin/sway
-      #  fi
-      #'';
+      profileExtra = ''
+        if [ -z $DISPLAY ] && [ "$(tty)" = "/dev/tty1" ] ; then
+          exec ${pkgs.sway}/bin/sway
+        fi
+      '';
       shellAliases = {
         cat = "bat";
       };
