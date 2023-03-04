ecd434f08f67c92f01119015e85dbde55df28c97
Adithya Nair <adtya@adtya.xyz>
2023-03-04 14:42:14 +0530

    Revert "ovmf with secureboot and tpm?"
    This reverts commit e76354c8c5266ae4b7d387ae61d1265eda8e2c2c.

--- system/virtualisation.nix
+++ system/virtualisation.nix
@@ -2,7 +2,7 @@
   virtualisation.docker.enable = true;
   virtualisation.libvirtd.enable = true;
   virtualisation.libvirtd.qemu.ovmf.enable = true;
-  virtualisation.libvirtd.qemu.ovmf.packages = [ (pkgs.OVMFFull.override { secureBoot = true; tpmSupport = true; }).fd ];
+  virtualisation.libvirtd.qemu.ovmf.packages = [ pkgs.OVMFFull.fd ];
   environment.systemPackages = with pkgs; [
     virt-manager
   ];
