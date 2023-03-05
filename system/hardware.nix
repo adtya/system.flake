f66ce722acc9a962769267243f23993d229f78e2
Adithya Nair <adtya@adtya.xyz>
2023-03-05 12:59:00 +0530

    filesystems: needed for boot

--- system/hardware.nix
+++ system/hardware.nix
@@ -6,16 +6,19 @@
       device = "tmpfs";
       fsType = "tmpfs";
       options = [ "defaults" "uid=0" "gid=0" "mode=0755" ];
+      neededForBoot = true;
     };
     "/nix" = {
       device = "/dev/system/root";
       fsType = "btrfs";
       options = [ "subvol=/@nix" "compress-force=zstd" ];
+      neededForBoot = true;
     };
     "/persist" = {
       device = "/dev/system/root";
       fsType = "btrfs";
       options = [ "subvol=/@persist" "compress-force=zstd" ];
+      neededForBoot = true;
     };
     "/mnt/system" = {
       device = "/dev/system/root";
