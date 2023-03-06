c74a87c105bf97707128255a14fcae3623d99195
Adithya Nair <adtya@adtya.xyz>
2023-03-07 02:22:42 +0530

    switch mako back to dracula

--- home/sway/mako.nix
+++ home/sway/mako.nix
@@ -4,12 +4,19 @@
   services.mako = {
     enable = true;
     font = "FiraCode Nerd Font 11";
-    backgroundColor = "#1E1E2E";
-    borderColor = "#89b4fa";
+    backgroundColor = "#282A36";
+    borderColor = "#282A36";
     progressColor = "over #313244";
+    textColor = "#44475A";
     extraConfig = ''
+      [urgency=low]
+      border-color=#282a36
+
+      [urgency=normal]
+      border-color=#f1fa8c
+
       [urgency=high]
-      border-color=#fab387
+      border-color=#ff5555
     '';
   };
 }
