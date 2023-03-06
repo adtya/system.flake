5ed6ca2971131453d95952f5eaf8ae0382c165f2
Adithya Nair <adtya@adtya.xyz>
2023-03-07 03:09:03 +0530

    use Newaita from package

--- home/sway/gtk.nix
+++ home/sway/gtk.nix
@@ -20,16 +20,9 @@ in
     package = pkgs.bibata-cursors;
   };
 
-  home.file.".local/share/icons".source = pkgs.fetchFromGitHub {
-    owner = "cbrnix";
-    repo = "Newaita";
-    rev = "c2b596b097a83be23833dc7bc40b5d07a63315e3";
-    hash = "sha256-tqtjUy8RjvOu0NaK+iE0R1g7/eqCpmhbdxuNGd/YfSI=";
-  };
-
   gtk.iconTheme = {
-    package = pkgs.catppuccin-papirus-folders.override { flavor = "mocha"; accent = "blue"; };
-    name = "Papirus";
+    package = pkgs.newaita-icons.override { variant = "dark"; panel = "dark"; folder = "bluegray"; };
+    name = "Newaita";
   };
 
   gtk.gtk3.extraConfig = {
