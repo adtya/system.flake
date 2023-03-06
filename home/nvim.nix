8ba81f75717546923ba19c598a7cf1ca9778fb16
Adithya Nair <adtya@adtya.xyz>
2023-03-07 02:28:27 +0530

    airline theme for nvim

--- home/nvim.nix
+++ home/nvim.nix
@@ -10,7 +10,7 @@
       {
         plugin = vim-airline;
         config = ''
-          let g:airline_theme='dracula'
+          let g:airline_theme='term'
           let g:airline#extensions#tabline#enabled=1
           let g:airline#extensions#tabline#formatter='unique_tail_improved'
           let g:airline_powerline_fonts = 1
