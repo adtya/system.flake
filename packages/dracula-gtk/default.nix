c41b1bc07adf9d01266b6f946b7baa748c9b15a9
Adithya Nair <adtya@adtya.xyz>
2023-03-06 23:27:23 +0530

    refactor dracula-gtk

--- packages/dracula-gtk/default.nix
+++ packages/dracula-gtk/default.nix
@@ -1,8 +1,8 @@
-{ lib, stdenv, fetchFromGitHub, gtk-engine-murrine }:
+{ lib, stdenv, fetchFromGitHub }:
 
 stdenv.mkDerivation {
   pname = "dracula-gtk";
-  version = "3.0";
+  version = "4.0";
 
   src = fetchFromGitHub {
     owner = "dracula";
@@ -11,15 +11,11 @@ stdenv.mkDerivation {
     sha256 = "sha256-7DUT/DfvCSo9GiJzqhqvegNCARB0eGtPQ6oUQG/Uja0=";
   };
 
-  propagatedUserEnvPkgs = [
-    gtk-engine-murrine
-  ];
-
   installPhase = ''
     runHook preInstall
 
     mkdir -p $out/share/themes/Dracula
-    cp -a * $out/share/themes/Dracula
+    cp -ra assets cinnamon gnome-shell gtk-2.0 gtk-3.0 gtk-3.20 gtk-4.0 metacity-1 unity xfwm4 index.theme $out/share/themes/Dracula/
 
     runHook postInstall
   '';
