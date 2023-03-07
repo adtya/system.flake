85f65d5179beeccc7c85793b26496272f1a8a9e1
Adithya Nair <adtya@adtya.xyz>
2023-03-08 00:30:48 +0530

    packagee catppuccin wallpapers

---
+++ packages/catppuccin-wallpapers/default.nix
@@ -0,0 +1,23 @@
+{ lib, stdenv, fetchFromGitHub }:
+
+stdenv.mkDerivation {
+  pname = "catppuccin-wallpapers";
+  version = "master";
+
+  src = fetchFromGitHub {
+    owner = "catppuccin";
+    repo = "wallpapers";
+    rev = "0cea4a28451851a637762dec07ec4fb2bfebc421";
+    hash = "sha256-B2ncT2qPc0inHHcO1BAZW5of+K0sIdtPcdpqcPUbKBo=";
+  };
+
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/share/wallpapers/catppuccin
+    find . -type f -regextype egrep -regex ".*\.(jpe?g|png)$" -exec cp {} $out/share/wallpapers/catppuccin/ \;
+
+    runHook postInstall
+  '';
+}
--- packages/default.nix
+++ packages/default.nix
@@ -3,4 +3,5 @@ self: super:
   adi1090x-plymouth = super.callPackage ./adi1090x-plymouth { };
   dracula-gtk = super.callPackage ./dracula-gtk { };
   newaita-icon-theme = super.callPackage ./newaita-icon-theme { };
+  catppuccin-wallpapers = super.callPackage ./catppuccin-wallpapers { };
 }
