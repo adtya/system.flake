a794dd7d863db93c6411c4f1741ea5be512a3858
Adithya Nair <adtya@adtya.xyz>
2023-03-10 20:42:10 +0530

    reconfigure zsh

--- home/terminal.nix
+++ home/terminal.nix
@@ -46,7 +46,6 @@
       };
       initExtra = ''
         bindkey -v '^?' backward-delete-char
-        unsetopt BEEP
       '';
       profileExtra = ''
         if [ -z $DISPLAY ] && [ "$(tty)" = "/dev/tty1" ] ; then
@@ -59,7 +58,7 @@
         grep = "grep --color=auto";
         ln = "ln -v";
         mv = "mv -v";
-        devenv_init = "nix flake init --template github:cachix/devenv";
+        devenv_flake = "nix flake init --template github:cachix/devenv";
         file = "nix run nixpkgs#file --";
         rebuild_system = "sudo nixos-rebuild --flake /persist/home/.system.flake# --cores 0 --max-jobs 8";
       };
