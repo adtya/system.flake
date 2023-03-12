e9d89404a99461524c135eb1bbff6f9a9c954412
Adithya Nair <adtya@adtya.xyz>
2023-03-12 17:10:20 +0530

    rotate password hashes

--- users/user.nix
+++ users/user.nix
@@ -1,9 +1,9 @@
 {
-  root.hashedPassword = "$y$j9T$xLZV3Xd.xBb3L2iXy1MPD/$eyvDDYu5lMhqXERgkpp0oBItiszp0rmh1siXAMpTJcB";
+  root.hashedPassword = "$6$/sCT1ZUkDwlPZ0U5$fDZbmOfABVmgiS0nlJq/9APEFmdlrwM15BI9PknIH1hrjhxjE7Ao/FnxKFeJB/amATNIE6iCm1W.J63SqozNZ.";
   primary = {
     userName = "adtya";
     realName = "Adithya Nair";
-    hashedPassword = "$y$j9T$WOLGsPx.0lb9nSD4Jf6mz1$SokbnAJ8RV/CmnKe5/8rBt66dRHcW.tFabV1q9ZWlPB";
+    hashedPassword = "$6$nYAbOJ71GE0aslmH$Zj7kjOZYZKbOnKG/lEeRTwydG.jp9N.wPR8GmxiN5i1FbeBrN7mUI3WAeQF4CyoK7g7J8aZmKWNSCzwcOFp9S/";
     emailAddress = "adtya@adtya.xyz";
     signingKey = "95ea0e6bf507e8ea346ac8740c7f35f4f821290f";
     emailUserName = "adtya@123mail.org";
